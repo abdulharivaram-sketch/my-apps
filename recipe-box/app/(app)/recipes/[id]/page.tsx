fa8073b47a4c61b95fe5dae719574621718e862b
abdulharivaram-sketch <abdul.harivaram@gmail.com>
2026-08-11 08:11:01 -0400

    Add files via upload

--- recipe-box/app/(app)/recipes/[id]/page.tsx
+++ recipe-box/app/(app)/recipes/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { notFound } from "next/navigation";
 import { getRecipe } from "@/lib/data/recipes";
+import { listFolders, getRecipeFolderIds } from "@/lib/data/folders";
 import CookingView from "@/components/recipe/CookingView";
 
 export default async function RecipePage({ params }: { params: { id: string } }) {
   const recipe = await getRecipe(params.id);
   if (!recipe) notFound();
-  return <CookingView recipe={recipe} />;
+  const [folders, folderIds] = await Promise.all([
+    listFolders(),
+    getRecipeFolderIds(params.id),
+  ]);
+  return <CookingView recipe={recipe} folders={folders} folderIds={folderIds} />;
 }
